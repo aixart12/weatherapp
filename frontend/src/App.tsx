18a3745458a8d5f5a1e3d278ee25843a18db99eb
Dhruv Kumar <dhruvlol12@gmail.com>
2022-05-18 12:02:33 +0530

    completed ui and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import Home from './component/Home';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home  />
+      < Home />
     </div>
+
   );
 }
 
